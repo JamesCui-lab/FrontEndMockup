bfa875fdda9f5745e197cbf2ab0ff9e65732ad9b
sherryxuxinxin <443728683@qq.com>
2019-07-25 17:16:31 +0800

    add the sort and page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,44 @@ import { DataService } from './data.service';
 })
 export class AppComponent implements OnInit{
   title = 'my-work';
-  people=[]
+  people=[];
+  malePets=[];
+  FemalePets = [];
   constructor(private dataService:DataService){}
   ngOnInit (){
     this.dataService.fetchData().subscribe(
-      (data)=> this.people = data
+      (data)=> {
+        this.people = data;
+    
+    for(var item in data){
+      if(data[item].gender=="Male"){
+        var petList = data[item].pets;
+        if(petList!=null){
+          for(var i in petList){
+            if(petList[i].type == "Cat"){
+              this.malePets.push(petList[i].name)
+            }
+          }
+        }
+          
+      }
+      else{
+        var petList = data[item].pets;
+        if(petList!=null){
+          for(var i in petList){
+            if(petList[i].type == "Cat"){
+              this.FemalePets.push(petList[i].name)
+            }
+          }
+        }
+      }
+  }
+  this.malePets.sort();
+  this.FemalePets.sort();
+      }
+      
     );
+    
+   
 }
 }
